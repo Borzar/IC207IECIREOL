77b1052305423bdd71e8343638b6855e02e42b4a
Boris <boris.zlobos@gmail.com>
2021-07-31 19:59:39 -0400

    validacion de campos y envio a PHP

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,10 +1,31 @@
 function validaCampos(){
 
     var nombre = document.getElementById("textNombre").value;
+    var categoria = document.getElementById("idCategoria").value;
+    var sucursal = document.getElementById("idSucursal").value;
+    var cantidad = document.getElementById("numCantidad").value;
+    var precio = document.getElementById("textPrecio").value;
+    
 
     if(nombre.length == 0 | /\s/.test(nombre)){
         alert("debe ingresar nombre");
         document.getElementById("textNombre").focus();
+        return false;
+    }else if(categoria == "") {
+        alert("debe seleccionar una categoria");
+        document.getElementById("idCategoria").focus();
+        return false;
+    }else if(sucursal == "") {
+        alert("debe seleccionar una sucursal");
+        document.getElementById("idSucursal").focus();
+        return false;
+    }else if (cantidad.length == 0 | /\s/.test(cantidad)){
+        alert("debe ingresar cantidad");
+        document.getElementById("numCantidad").focus();
+        return false;
+    }else if (precio.length == 0 | /\s/.test(precio)){
+        alert("debe ingresar precio");
+        document.getElementById("textPrecio").focus();
         return false;
     }
 
--- recibeDatos.php
+++ recibeDatos.php
@@ -1,5 +1,20 @@
 <?php
 
 $nombre = $_POST['nombre'];
+$categoria = $_POST['categoria'];
+$sucursal = $_POST['sucursal'];
+$descripcion = $_POST['descripcion'];
+$cantidad = $_POST['cantidad'];
+$precio = $_POST['precio'];
+
 
 print "El nombre recibido es ".$nombre;
+print '<br>';
+print "La categoria recibida es ".$categoria;
+print '<br>';
+print "La sucursal recibida es ".$sucursal;
+print '<br>';
+print "La cantidad recibida es ".$cantidad;
+print '<br>';
+print "El precio recibido es ".$precio;
+print '<br>';
